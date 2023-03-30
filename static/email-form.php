b1611f1277584771fc1f21b4bd8167e77b63c260
AK <kraft.aric@gmail.com>
2023-03-30 13:25:58 -0700

    add isInjected function to php

--- static/email-form.php
+++ static/email-form.php
@@ -17,13 +17,13 @@
         exit;
     }
 
-    // $email_from = '<arickraft.me>';
+    $email_from = '<arickraft.me>';
     $email_subject = "Portfolio Form submission";
     $email_body = "New message from $name.\n".
         "Message:\n $message".
         
     $to = "<kraft.aric@gmail.com>";
-    $headers = "From: Portfolio \r\n";
+    $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
 
     //Send email
@@ -32,5 +32,29 @@
     //redirect to thank-you page.
     echo "Thank you for reaching out";
     exit;
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+
 
 ?>
