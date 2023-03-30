adb16a268ff2fb86c3ef7191534de4aa364bc6a1
AK <kraft.aric@gmail.com>
2023-03-30 12:24:40 -0700

    added php

---
+++ html/email-form.php
@@ -0,0 +1,36 @@
+<?php 
+    
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    //Validate 
+    if(empty($name)||empty($visitor_email)) 
+    {
+        echo "Please enter your name and a valid email";
+        exit;
+    }
+
+    if(IsInjected($visitor_email))
+    {
+        echo "Invalid email";
+        exit;
+    }
+
+    $email_from = '<http://127.0.0.1:5500/html/contactMe.html>';
+    $email_subject = "Portfolio Form submission";
+    $email_body = "New message from $name.\n".
+        "Message:\n $message".
+        
+    $to = "<kraft.aric@gmail.com>";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    //Send email
+    mail($to,$email_subject,$email_body,$headers);
+
+    //redirect to thank-you page.
+    echo "Thank you for reaching out";
+    exit;
+
+?>
